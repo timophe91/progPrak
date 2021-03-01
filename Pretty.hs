60c2005d5bfb76e2040d8baf3a961504866d8d2d
timophe91 <74023447+timophe91@users.noreply.github.com>
2021-03-01 13:00:25 +0100

    aufgabe 2

--- Pretty.hs
+++ Pretty.hs
@@ -2,9 +2,12 @@ import Type
 
 pretty :: Term -> String 
 pretty (Var (VarName v)) = v
-pretty (Comb "." [t1,t2]) = 
+pretty (Comb "." [t1,t2]) = prettyCombination
 pretty (Comb c []) = c
 pretty (Comb c t) = c ++ "(" ++ coma (map pretty t) ++ ")"
   where coma:: [String] -> String
         coma [x]    = x
         coma (x:xs) = (x ++ ", ") ++ coma xs
+
+
+prettyCombination :: Term -> Term -> String 
