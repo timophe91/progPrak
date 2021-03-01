01b90e0fe34a60de8a580ff5f01ac93c5cf2ae90
timophe91 <74023447+timophe91@users.noreply.github.com>
2021-03-01 11:09:02 +0100

    pretty

--- Pretty.hs
+++ Pretty.hs
@@ -1,2 +1,7 @@
 import Type
 
+class Pretty p where 
+    pretty :: a -> String
+    pretty :: Term -> String
+    pretty Var (VarName a) = a
+    pretty Comb (CombName [t:ts]) = pretty t  ++ pretty (Comb (CombName ts))
