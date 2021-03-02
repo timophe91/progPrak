f402a972e5773ceed5debe5d9038325020d0c5aa
Corvin <gridulin@gmail.com>
2021-03-02 11:15:39 +0100

    merge

--- Variables.hs
+++ Variables.hs
@@ -14,7 +14,6 @@ instance Vars Term where
 instance Vars Rule where
     allVars (Rule x t) = allVars x ++ concatMap allVars t
 
-<<<<<<< HEAD
 instance Vars Prog where
     allVars (Prog r) = concatMap allVars r
 
@@ -24,15 +23,8 @@ instance Vars Goal where
 
 
 
-{-freshVars :: [VarName]    
-freshVars = do
-    a <- [0..]
-    b <- ['A'..'Z']
-    return (VarName <$> a b)-}
-=======
 {- Generating a endless list of VarNames with the form:
  - [VarName "A", ... ,VarName "Z", VarName "A0", ... ,VarName "Z0", VarName "A1", ... , VarName "Z1", ... ]
 -}
 freshVars :: [VarName]    
 freshVars = [VarName [x] | x <- ['A'..'Z']] ++ [VarName (l:show x) | x <- [0..],  l <- ['A'..'Z']]
->>>>>>> 02492f6e415e30f25c3cb44cfb8bebfa7a145692
