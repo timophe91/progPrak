446f4d5850810b6a16280052dc2b55ded2439405
Corvin <gridulin@gmail.com>
2021-03-02 14:56:40 +0100

    Substitution

--- Substitution.hs
+++ Substitution.hs
@@ -5,6 +5,8 @@ import Variables
 
 type Subst = [(Term, Term)]
 
+{- outputs the domain of a substitution
+-}
 domain :: Subst -> [VarName]
 domain s = concatMap allVars (right s)
   where
@@ -12,12 +14,19 @@ domain s = concatMap allVars (right s)
   right []        = []
   right ((x,y):r) = y : right r
 
+{- Creates a empty substitution
+-}
 empty :: Subst
 empty = []
 
+{- Creates a substitution that maps a single variable to a term
+-}
 single :: VarName -> Term -> Subst
 single v t = [(Var v, t)]
 
+{- Applys a substitution to a given term
+-}
 apply :: Subst -> Term -> Term
-apply []        t = t
-apply ((x,y):s) t = if x == t then y else apply s t
+apply []            t          = t
+apply ((Var x,y):s) (Var v)    = if x == v then y else apply s (Var v)
+apply s             (Comb n t) = Comb n [apply s x| x <- t]
