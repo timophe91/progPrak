b14ea28884fdd3667b8f932ee6e9047ba5c1d0b0
Corvin <gridulin@gmail.com>
2021-03-02 11:01:10 +0100

    allVars

--- Variables.hs
+++ Variables.hs
@@ -7,12 +7,23 @@ class Vars v where
 
 
 instance Vars Term where
-    allVars  (Var v) = [v]
+    allVars (Var v)    = [v]
+    allVars (Comb _ t) = concatMap allVars t
+
+instance Vars Rule where
+    allVars (Rule x t) = allVars x ++ concatMap allVars t
+
+instance Vars Prog where
+    allVars (Prog r) = concatMap allVars r
+
+instance Vars Goal where
+    allVars (Goal t) = concatMap allVars t
 
 
 
-freshVars :: [VarName]    
+
+{-freshVars :: [VarName]    
 freshVars = do
     a <- [0..]
     b <- ['A'..'Z']
-    return (VarName <$> a b)
+    return (VarName <$> a b)-}
