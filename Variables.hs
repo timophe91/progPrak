02492f6e415e30f25c3cb44cfb8bebfa7a145692
timophe91 <74023447+timophe91@users.noreply.github.com>
2021-03-02 11:00:13 +0100

    Endless freshVars

--- Variables.hs
+++ Variables.hs
@@ -1,6 +1,7 @@
 module Variables where
 
 import Type
+import Control.Monad
 
 class Vars v where
     allVars :: v -> [VarName]
@@ -10,9 +11,8 @@ instance Vars Term where
     allVars  (Var v) = [v]
 
 
-
+{- Generating a endless list of VarNames with the form:
+ - [VarName "A", ... ,VarName "Z", VarName "A0", ... ,VarName "Z0", VarName "A1", ... , VarName "Z1", ... ]
+-}
 freshVars :: [VarName]    
-freshVars = do
-    a <- [0..]
-    b <- ['A'..'Z']
-    return (VarName <$> a b)
+freshVars = [VarName [x] | x <- ['A'..'Z']] ++ [VarName (l:show x) | x <- [0..],  l <- ['A'..'Z']]
