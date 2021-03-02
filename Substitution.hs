f7d8c4e08fc2bb3ecb7d4cc411dcd0da906a76b0
Corvin <gridulin@gmail.com>
2021-03-02 16:37:37 +0100

    Substitution

--- Substitution.hs
+++ Substitution.hs
@@ -8,11 +8,9 @@ type Subst = [(VarName , Term)]
 {- outputs the domain of a substitution
 -}
 domain :: Subst -> [VarName]
-domain s = left s
-  where
-  left :: Subst -> [VarName]
-  left []        = []
-  left ((x,y):l) = if x /= y then x : left l else left l
+domain []            = []
+domain ((x,Var v):s) = if x /= v then x : domain s else domain s
+domain ((x,_):s)     = x : domain s
 
 {- Creates a empty substitution
 -}
@@ -38,3 +36,9 @@ compose x  y  =
   where
   help (x1,y1) []           = []
   help (x1,y1) ((x2,y2):s2) = if y2 == x1 then (x2,y1) : help (x1,y2) s2 else (x2,y2) : help (x1,y1) s2-}
+
+{- Restricts the domain of a substitution to a given set of variables
+-}
+restrictTo :: Subst -> [VarName] -> Subst
+restrictTo []        _ = []
+restrictTo ((x,y):s) n = if x `elem` n then (x,y) : restrictTo s n else restrictTo s n
