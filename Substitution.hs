ad1c4d6dd354c29ce6ab36507fedd376c0061366
timophe91 <74023447+timophe91@users.noreply.github.com>
2021-03-03 11:26:01 +0100

    Subs als Data Typ, damit es beim instanziieren weniger probleme gibt

--- Substitution.hs
+++ Substitution.hs
@@ -3,33 +3,36 @@ module Substitution where
 import Type
 import Variables
 
-type Subst = [(VarName , Term)]
+--type Subst = [(VarName , Term)]
+data Subst = Empty | Subs [(VarName, Term)]
+  deriving (Eq, Show)
 
 {- outputs the domain of a substitution
+TODO: abbildung auf sich selbst müssen nicht mehr untersucht werden
 -}
 domain :: Subst -> [VarName]
-domain []            = []
-domain ((x,Var v):s) = if x /= v then x : domain s else domain s
-domain ((x,_):s)     = x : domain s
+domain Empty                = []
+domain (Subs ((x,Var v):s)) = if x /= v then x : domain (Subs s) else domain (Subs s)
+domain (Subs ((x,_):s))     = x : domain (Subs s)
 
 {- Creates a empty substitution
 -}
 empty :: Subst
-empty = []
+empty = Empty
 
 {- Creates a substitution that maps a single variable to a term
  - substitutions on self r empty
 -}
 single :: VarName -> Term -> Subst
-single v (Var v2) = if v == v2 then empty else [(v, Var v2)]
-single v t        = [(v, t)]
+single v (Var v2) = if v == v2 then empty else Subs [(v, Var v2)]
+single v t        = Subs [(v, t)]
 
 {- Applys a substitution to a given term
 -}
 apply :: Subst -> Term -> Term
-apply []            t          = t
-apply ((x,y):s) (Var v)    = if x == v then y else apply s (Var v)
-apply s             (Comb n t) = Comb n [apply s x| x <- t]
+apply Empty      t                = t
+apply (Subs ((x,y):s)) (Var v)    = if x == v then y else apply (Subs s) (Var v)
+apply s             (Comb n t)    = Comb n [apply s x| x <- t]
 
 {-compose :: Subst -> Subst -> Subst
 compose s1 [] = s1
@@ -42,5 +45,5 @@ compose x  y  =
 {- Restricts the domain of a substitution to a given set of variables
 -}
 restrictTo :: Subst -> [VarName] -> Subst
-restrictTo []        _ = []
-restrictTo ((x,y):s) n = if x `elem` n then (x,y) : restrictTo s n else restrictTo s n
+restrictTo (Subs []) _        = Empty
+restrictTo (Subs ((x,y):s)) n = if x `elem` n then  compose (single x,y) restrictTo s n else restrictTo (Subs s) n
