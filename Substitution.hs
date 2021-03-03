afaed0e84d18b4575117d7d4bac74c05e1025887
timophe91 <74023447+timophe91@users.noreply.github.com>
2021-03-03 11:43:38 +0100

    ich hasse haskell und prolog

--- Substitution.hs
+++ Substitution.hs
@@ -3,7 +3,7 @@ module Substitution where
 import Type
 import Variables
 
---type Subst = [(VarName , Term)]
+--type Subst = [(VarName , Term)] kann weg
 data Subst = Empty | Subs [(VarName, Term)]
   deriving (Eq, Show)
 
@@ -12,7 +12,7 @@ TODO: abbildung auf sich selbst müssen nicht mehr untersucht werden
 -}
 domain :: Subst -> [VarName]
 domain Empty                = []
-domain (Subs ((x,Var v):s)) = if x /= v then x : domain (Subs s) else domain (Subs s)
+--domain (Subs ((x,Var v):s)) = if x /= v then x : domain (Subs s) else domain (Subs s) kann weg
 domain (Subs ((x,_):s))     = x : domain (Subs s)
 
 {- Creates a empty substitution
@@ -34,13 +34,10 @@ apply Empty      t                = t
 apply (Subs ((x,y):s)) (Var v)    = if x == v then y else apply (Subs s) (Var v)
 apply s             (Comb n t)    = Comb n [apply s x| x <- t]
 
-{-compose :: Subst -> Subst -> Subst
-compose s1 [] = s1
-compose [] s2 = s2
-compose x  y  = 
-  where
-  help (x1,y1) []           = []
-  help (x1,y1) ((x2,y2):s2) = if y2 == x1 then (x2,y1) : help (x1,y2) s2 else (x2,y2) : help (x1,y1) s2-}
+compose :: Subst -> Subst -> Subst
+compose s1       Empty    = s1
+compose Empty    s2       = s2
+compose (Subs x) (Subs y) = 
 
 {- Restricts the domain of a substitution to a given set of variables
 -}
