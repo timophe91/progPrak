f2a0580a00fda173bd785a8f4b78ffe5a149cbef
timophe91 <74023447+timophe91@users.noreply.github.com>
2021-03-03 14:59:45 +0100

    compose jetzt richtig... vll

--- Substitution.hs
+++ Substitution.hs
@@ -2,16 +2,13 @@ module Substitution where
 
 import Type
 
---type Subst = [(VarName , Term)] kann weg
 data Subst = Empty | Subs [(VarName, Term)]
   deriving (Eq, Show)
 
 {- outputs the domain of a substitution
-TODO: abbildung auf sich selbst müssen nicht mehr untersucht werden
 -}
 domain :: Subst -> [VarName]
 domain Empty                = []
---domain (Subs ((x,Var v):s)) = if x /= v then x : domain (Subs s) else domain (Subs s) kann weg
 domain (Subs ((x,_):s))     = x : domain (Subs s)
 
 {- Creates a empty substitution
@@ -34,23 +31,31 @@ apply (Subs [])  t                = t
 apply (Subs ((x,y):s)) (Var v)    = if x == v then y else apply (Subs s) (Var v)
 apply s             (Comb n t)    = Comb n [apply s x| x <- t]
 
+{- Merge two Prolog Substitutions
+-}
 compose :: Subst -> Subst -> Subst
 compose s1    Empty             = s1
 compose Empty s2                = s2
 compose (Subs [])     s2        = Empty
 compose s1    (Subs [])         = Empty
-compose (Subs (y:ys)) s2 = concatSubs (composeHelp (Subs [y]) s2) (compose (Subs ys) s2)
+compose (Subs (su:sus)) s2 = concatSubs (composeHelp (Subs [su]) s2) (compose (Subs sus) s2)
   where
     composeHelp :: Subst -> Subst -> Subst
     composeHelp (Subs [(v, t)]) (Subs ((v2, t2):ys))
-     | v == v2     = concatSubs (single v2 (apply (Subs [(v, t)]) t2)) (compose (Subs [(v, t)]) (Subs ys))
-     | t == Var v2 = concatSubs (single v (apply (Subs [(v, t)]) t2)) (compose (Subs [(v, t)]) (Subs ys))
-     | otherwise   = concatSubs (compose (single v2 (apply (Subs [(v, t)]) t2)) (compose (Subs [(v, t)]) (Subs ys))) (single v t)
+     | v == v2     = concatSubs (compose (Subs [(v, t)]) (Subs ys)) (single v2 (apply (Subs [(v, t)]) t2)) -- if two VarName r the same: A -> B, A -> C
+     | t == Var v2 = concatSubs (compose (Subs [(v, t)]) (Subs ys)) (single v  (apply (Subs [(v, t)]) t2)) -- if u have A -> B , B -> C
+     | otherwise   = concatSubs (concatSubs (single v t) (single v2 (apply (Subs [(v, t)]) t2))) (compose (Subs [(v, t)]) (Subs ys)) -- every other case
 
+{- Concatenate two Subst and removing duplicates
+-}
 concatSubs :: Subst -> Subst -> Subst
-concatSubs Empty x           = x
-concatSubs x     Empty       = x
-concatSubs (Subs x) (Subs y) = Subs (x ++ y)
+concatSubs Empty    x           = x
+concatSubs x        Empty       = x
+concatSubs (Subs x) (Subs y)    = Subs (removeDups (x ++ y))
+  where 
+    removeDups []     = []
+    removeDups (x:xs) = if x `elem` xs then removeDups xs else x : removeDups xs
+
 
 {- Restricts the domain of a substitution to a given set of variables
 -}
