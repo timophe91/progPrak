afcd657db6278b75832464bfcc2081ac194b7efe
timophe91 <74023447+timophe91@users.noreply.github.com>
2021-03-05 18:55:29 +0100

    lol

--- Rename.hs
+++ Rename.hs
@@ -12,5 +12,5 @@ rename v  r = renameVars v  (allVars r) r
 
 renameVars :: [VarName] -> [VarName]-> Rule -> Rule
 renameVars v vR (Rule t ts)  = let var  = (freshVars \\ (v ++ vR)) 
-                                   subs = foldr compose empty (zipWith (\varN1 varN2 -> single varN1 (Var varN2))  vR var)
+                                   subs = compose (foldr compose empty (zipWith (\varN1 varN2 -> single varN1 (Var varN2))  vR var)) (single (VarName "_") (Var (VarName "_r")))
                                in  Rule (apply subs t) (map (apply subs) ts)
