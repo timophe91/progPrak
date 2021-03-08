494194409fb912110d49541d24e5eabab2e98f35
timophe91 <74023447+timophe91@users.noreply.github.com>
2021-03-08 17:26:41 +0100

    asdf

--- Interactive.hs
+++ Interactive.hs
@@ -7,29 +7,29 @@ import Data.Either
 interactive :: IO()
 interactive = do
     putStrLn welcomeWaggon
-    looping
-    p
+    looping -- loop  it
+    putStrLn quitWaggon 
     return()
 
 looping :: IO()
 looping = do
-    x <- readFromUser     
-    unless (x /= ":q")
+    x <- readFromUser -- read user input
+    unless (x /= ":q") -- do the loop until it quits
       $ printResponse (evalAction parseLine) >> looping
       
 
 evalAction :: Char -> String -> String
 evalAction c l | c == 'h' = helpWaggon
-               | c == 'l' = let r = parseFile l --
+               | c == 'l' = let r = parseFile l -- parse ´from filepath
                             in if not (null (fromRight r)) then "Loaded." else fromLeft r -- hm
-               | c == 'g' = let g = parse l 
-                            in "TODO: Implement handle goal"
-               | c == 'r' =  
+               | c == 'g' = let g = parse l -- parse a goal
+                            in "TODO: Implement handle goal" 
+               | c == 'q' = "Please Quit with ':q'" -- irgendwas stimmte nicht.
                              
 
 parseLine :: String -> (Char, String)
-parseLine (s:rs) | s == ':' = (head rs, dropWhile isSpace (tail rs))
-                 | otherwise = ('g', s:rs) 
+parseLine (s:rs) | s == ':' = (head rs, dropWhile isSpace (tail rs)) -- parse the first char after : and remove whitespaces
+                 | otherwise = ('g', s:rs) otherwise it should be a goal 
 
 
 readFromUser :: IO String
