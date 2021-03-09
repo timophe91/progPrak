4752321fc29ff8ea6bfd3d74511ab27411087523
Corvin <gridulin@gmail.com>
2021-03-09 11:42:59 +0100

    sld

--- SLD.hs
+++ SLD.hs
@@ -9,9 +9,25 @@ newtype Edge = Edge Subst
 data SLDTree = Node Goal Edge [SLDTree]
   deriving (Show)
 
+type Strategy = SLDTree -> [Subst]
+
+dfs :: Strategy
+dfs = dfss
+  where
+  dfss :: SLDTree -> [Subst]
+  dfss s = []
+
+bfs :: Strategy
+bfs = bfss
+  where
+  bfss :: SLDTree -> [Subst]
+  bfss s = []
+
+solveWith :: Prog -> Goal -> Strategy -> [Subst]
+solveWith p g s = []
+
 sld :: Prog -> Goal -> SLDTree
 sld p g = Node g (Edge empty) (newTree p g)
   where
   newTree :: Prog -> Goal -> [SLDTree]
-  newTree (Prog (Rule t x:ps)) g = tree (Rule t x) g : newTree (Prog ps) g
-  tree r g = Node g (Edge empty) []
+  newTree p g = []
