db34031c9ea91ac395c1f1185516717a378c8cc8
timophe91 <74023447+timophe91@users.noreply.github.com>
2021-03-10 13:39:20 +0100

    Interacite nochmal ganz neu. jetzt gehts.

--- Interactive.hs
+++ Interactive.hs
@@ -12,73 +12,73 @@ import Substitution
 -- Interactive REPL for a simple Prolog
 interactive :: IO()
 interactive = do
-    putStrLn welcomeWaggon
-    looping "" (Prog []) dfs -- loop  it
-    putStrLn quitWaggon
+  putStrLn welcomeWaggon
+  readCommand dfs "" (Left "Program started, no File Loaded.")
 
--- 
-looping :: String -> Prog -> Strategy -> IO()
-looping s p strat = do
-    x <- readFromUser -- read user input
-    let (a, str)   = parseLine x
-    let (rC, progr, gol, stratN, response) | a == 'l'  = evalActionLoad str 
-                                           | a == 'r'  = evalActionLoad s
-                                           | otherwise = evalAction a str
-    case rC of  
-       4 -> do -- solve a goal
-              solveGoal p gol strat
-              looping s p strat 
-       3 -> return() -- byebye
-       1 -> looping str progr strat -- loaded a Prog from File
-       5 -> do
-              putStrLn response
-              looping s p stratN -- new Strat
-       _ -> do -- print and loop -> error, help message
-              putStrLn response     
-              looping s p strat                      
+      where
+        -- Read a command and pipe it to eval
+        readCommand :: Strategy -> String ->  Either String Prog -> IO()
+        readCommand  strat filePath eProg = do putStr "?- "
+                                               c <- getLine -- read from console
+                                               eval strat c filePath eProg
+        eval :: Strategy -> String -> String -> Either String Prog -> IO()
+        -- call the help Waggon
+        eval strat' ":h"   filePath' eProg' = do putStrLn helpWaggon
+                                                 readCommand strat' filePath' eProg' -- loop
+        -- reload prog
+        eval strat' ":r"   filePath' eProg' = do loadedFile <- parseFile filePath'
+                                                 case loadedFile of
+                                                   Left  errStr''' -> do putStrLn errStr''' -- error but keep Path and failed Prog
+                                                                         readCommand strat' filePath' (Left  errStr''') -- loop with new error
+                                                   Right eProg'''  -> do putStrLn "Reloaded."
+                                                                         readCommand strat' filePath' (Right eProg''') -- loop with reloaded prog
+        
+        -- end program
+        eval _      ":q"   _         _      = putStrLn quitWaggon -- quit
+        -- evaluate user input
+        eval strat' s      filePath' eProg' = let (action:userInput) = words s -- split words by space or newline
+                                              in case length userInput of 
+                                                   0 ->  let evalGoal = parse action -- the is only one command, and its the goal
+                                                         in case evalGoal of -- check Goal for error
+                                                              Left  errStr  -> do putStrLn "Error Parsing Goal:"
+                                                                                  putStrLn errStr -- error in parsing Goal
+                                                                                  readCommand strat' filePath' eProg' -- loop 
+                                                              Right rdGoal -> case eProg' of -- check if Prog is loaded correctly 
+                                                                                Left  errStr' -> do putStrLn "Error Parsing Prog:"
+                                                                                                    putStrLn errStr'
+                                                                                                    readCommand strat' filePath' eProg' -- loop
+                                                                                Right rdProg  -> do printSolutions (solveWith rdProg rdGoal strat')
+                                                   1 -> case action of
+                                                          -- load a file
+                                                          ":l" -> do loadedFile <- parseFile (head userInput)
+                                                                     case loadedFile of
+                                                                       Left  errStr'' -> do putStrLn errStr'' -- error but keep Path and failed Prog
+                                                                                            readCommand strat' (head userInput) (Left  errStr'') -- loop with loaded error and failed path
+                                                                       Right eProg''  -> do putStrLn "Loaded."
+                                                                                            readCommand strat' (head userInput) (Right eProg'') -- loop with new prog and succeded path
+                                                          -- change the strategy
+                                                          ":s" | head userInput == "dfs" -> do putStrLn "Strategy set to depth-first search."
+                                                                                               readCommand dfs filePath' eProg' -- loop with dfs as strat
+                                                               | head userInput == "bfs" -> do putStrLn "Strategy set to breadth-first search."
+                                                                                               readCommand bfs filePath' eProg' -- loop with bfs as strat
+                                                               | otherwise               -> do putStrLn "The Strategy u r looking for was not found.\nType ':h' for help."
+                                                                                               readCommand strat' filePath' eProg' -- loop with old strat
+                                                          -- corrent number of commands, but command not available
+                                                          _    -> do putStrLn "Command not found. \nType ':h' for help."
+                                                                     readCommand strat' filePath' eProg' -- loop
+                                                  -- wrong number of commands and input
+                                                   _    -> do putStrLn "Command not found. \nType ':h' for help."
+                                                              readCommand strat' filePath' eProg' -- loop
 
-solveGoal :: Prog -> Goal -> Strategy -> IO()
-solveGoal p g s = do 
-                    let a = solveWith p g s
-                    printIfWanted a
-
-printIfWanted :: [Subst] -> IO ()
-printIfWanted s = case s of 
-                   []     -> putStrLn ""
+-- Pretty Printing the Solutions
+printSolutions :: [Subst] -> IO ()
+printSolutions s = case s of 
+                   []     -> putStrLn "No more Solutions found."
                    (s:rs) -> do 
                               putStrLn (pretty s) -- pretty output the substitution
                               c <- getChar        -- get next action
-                              when (c == ';') $ printIfWanted rs -- as long as ';' is used, continue 
-      
-evalActionLoad :: String -> (Integer, Prog, Goal, Strategy, String)
-evalActionLoad l = do 
-                     let r = parseFile l -- parse from filepath
-                     case r of
-                       Left  leftE  -> (-1, Prog [], Goal [], dfs, leftE)
-                       Right rightE -> (1, rightE :: Prog, Goal [], dfs, "Loaded.")
-
-evalAction :: Char -> String -> (Integer, Prog, Goal, Strategy, String)
-evalAction c l | c == 'h' = (2, Prog[], Goal [], dfs, helpWaggon)
-               | c == 'q' = (3, Prog[], Goal [], dfs, "")
-               | c == 'g' = let r = parse l -- parse a goal
-                            in case r of
-                                 Left leftE -> (-1, Prog [], Goal [], dfs, leftE)
-                                 Right rightE -> (4, Prog[], rightE, dfs, "")
-               | c == 's' = case l of 
-                              "dfs" -> (5, Prog [], Goal [], dfs, "Strategy set to deepth-first search.")
-                              "bfs" -> (5, Prog [], Goal [], bfs, "Strategy set to breadth-first search.")
-                              _     -> (-1, Prog [], Goal [], bfs, "Error: Strategy not found.")
-               | otherwise  = (-1, Prog [], Goal [], dfs, "Error: Something went Wrong, probably wrong Command.\nType :h for Help.")
-
-parseLine :: String -> (Char, String)
-parseLine (s:rs) | s == ':' = (head rs, dropWhile isSpace (tail rs)) -- parse the first char after : and remove whitespaces
-                 | otherwise = ('g', s:rs) -- otherwise it should be a goal 
-
-
-readFromUser :: IO String
-readFromUser = putStrLn  "?- "
-            >> getLine 
-
+                              when (c == ';') $ printSolutions rs -- as long as ';' is used, continue 
+-- just the help message
 helpWaggon :: String 
 helpWaggon = unlines 
   [
@@ -90,8 +90,10 @@ helpWaggon = unlines
      ,":r          Reloads the last loaded file."
      ,":s <strat>  Sets the specified search strategy"
      ,"            where <strat> is one of 'dfs', 'bfs'."
+     ,"            default is 'dfs'"
   ]
 
+-- welcome message
 welcomeWaggon :: String
 welcomeWaggon = unlines
   [ "##################################"
@@ -100,6 +102,7 @@ welcomeWaggon = unlines
    ,"##################################"
   ]
 
+-- quit message
 quitWaggon :: String
 quitWaggon = unlines 
   [
