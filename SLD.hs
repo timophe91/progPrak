d01276c8c559049b9b10842ae5e9db11905ab120
Corvin <gridulin@gmail.com>
2021-03-10 13:59:49 +0100

    sld

--- SLD.hs
+++ SLD.hs
@@ -2,11 +2,11 @@ module SLD where
 
 import Substitution
 import Type
+import Rename
+import Unifikation
+import Data.Maybe
 
-newtype Edge = Edge Subst
-  deriving (Show, Eq)
-
-data SLDTree = Node Goal Edge [SLDTree]
+data SLDTree = Node Goal [(Subst, SLDTree)]
   deriving (Show)
 
 type Strategy = SLDTree -> [Subst]
@@ -27,7 +27,9 @@ solveWith :: Prog -> Goal -> Strategy -> [Subst]
 solveWith p g s = []
 
 sld :: Prog -> Goal -> SLDTree
-sld p g = Node g (Edge empty) (newTree p g)
+sld (Prog (x:xs)) g = let p1 = Prog [rename (allVars g) p| p <- x:xs] in Node g (newTree p1 g p1)
   where
-  newTree :: Prog -> Goal -> [SLDTree]
-  newTree p g = []
+  newTree :: Prog -> Goal -> Prog ->[(Subst, SLDTree)]
+  newTree (Prog [])               _             _ = []
+  newTree (Prog (Rule xt []:xs))  (Goal (y:ys)) p = let s = unify xt y in if isJust s then let g1 = Goal [apply (fromJust s) z | z <- y:ys] in (fromJust s, Node (Goal [apply (fromJust s) xt]) []) : newTree (Prog xs) (Goal (y:ys)) p else newTree (Prog xs) (Goal (y:ys)) p
+  newTree (Prog (Rule xt xts:xs)) (Goal (y:ys)) p = let s = unify xt y in if isJust s then let g1 = Goal [apply (fromJust s) z | z <- y:ys] in (fromJust s, Node (Goal (map (apply (fromJust s)) xts)) (newTree (Prog xs) g1 p)) : newTree (Prog xs) (Goal (y:ys)) p else newTree (Prog xs) (Goal (y:ys)) p
