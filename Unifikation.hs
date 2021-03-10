edd56fc962fa13fe5369721e4662fcef06bbaf58
timophe91 <74023447+timophe91@users.noreply.github.com>
2021-03-10 19:44:54 +0100

    asdf

--- Unifikation.hs
+++ Unifikation.hs
@@ -11,7 +11,7 @@ import Data.Maybe
 ds :: Term -> Term -> Maybe (Term, Term)
 ds (Var (VarName "_")) _                   = Nothing -- If one of the Variables is anonymous return Nothing
 ds _                   (Var (VarName "_")) = Nothing
-ds (Var v)             t                   = if Var v == t then Nothing else Just (Var v, t) -- If one of the terms is a variable and the terms a not equal return the tuple of both terms
+ds (Var v)             t                   = if Var v == t then Nothing else Just (Var v, t) -- If one of the terms is a variable and the terms r not equal return the tuple of both terms
 ds t                   (Var v)             = if Var v == t then Nothing else Just (Var v, t) -- for unify changed order
 ds (Comb v1 t1)        (Comb v2 t2)        = if v1 /= v2 || length t1 /= length t2 then Just (Comb v1 t1, Comb v2 t2) else help t1 t2 -- If both terms are funktions with the same name and number of terms in the list return the first element which differs
   where
@@ -26,7 +26,7 @@ ds (Comb v1 t1)        (Comb v2 t2)        = if v1 /= v2 || length t1 /= length 
 {- Calculates the most general unifier of two terms
 -}
 unify :: Term -> Term -> Maybe Subst
-unify x y = if x == y then Just empty else help x y empty
+unify x y = if x == y then Just empty else help x y empty -- terms r equal, 
   where
   help :: Term -> Term -> Subst -> Maybe Subst
   help t1 t2 s = let d = ds (apply s t1) (apply s t2) in if isNothing d then Just s else help2 t1 t2 d s -- calculate the ds and generate the new substitution if ds is not Nothing
